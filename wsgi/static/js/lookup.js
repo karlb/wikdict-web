9128ee3c0a6ddbe1c910345efee484b6934d3e96
Karl Bartel <karl42@gmail.com>
2016-01-02 16:05:06 +0100

    merge suggestions for both languages

--- wsgi/static/js/lookup.js
+++ wsgi/static/js/lookup.js
@@ -13,31 +13,52 @@ $(function() {
     })
 
     // typeahead
-    var substringMatcher = function(strs) {
+    var substringMatcher = function() {
         var loaded_prefix;
+        var loaded_langs;
+        //var merged_and_sorted;
 
-        function matches(q) {
-            return $.grep(strs, function(x) {
+        function match(q) {
+            return $.grep(merged_and_sorted, function(x) {
                 return x[0].toLowerCase().startsWith(q.toLowerCase());
+            });
+        }
+
+        function load_lang(lang, prefix, q, cb_async) {
+            //$.ajax('http://127.0.0.1:8080/' + lang + '/' + encodeURI(encodeURI(prefix.toLowerCase())) + '.json', {
+            $.ajax('http://storage.googleapis.com/wikdict-cdn/typeahead2/' + lang + '/' + encodeURI(encodeURI(prefix.toLowerCase())) + '.json', {
+                success: function (data) {
+                    loaded_langs[lang] = data;
+                    if (loaded_langs[from_lang] && loaded_langs[to_lang]) {
+                        // add language info
+                        $.each(loaded_langs[from_lang], function(i, x) {
+                            x.push(from_lang);
+                        });
+                        $.each(loaded_langs[to_lang], function(i, x) {
+                            x.push(to_lang);
+                        });
+                        // merge and sort
+                        merged_and_sorted = loaded_langs[from_lang].concat(loaded_langs[to_lang]);
+                        merged_and_sorted.sort(function (a, b) {return b[1] - a[1]});
+
+                        cb_async(match(q));
+                        loaded_prefix = prefix;
+                    }
+                }
             });
         }
 
         return function findMatches(q, cb, cb_async) {
             var prefix = q.slice(0, 3);
-            if (loaded_prefix != prefix) {
-                strs = [];
-                $.ajax('http://127.0.0.1:8080/' + from_lang + '/' + encodeURI(encodeURI(prefix.toLowerCase())) + '.json', {
-                //$.ajax('http://storage.googleapis.com/wikdict-cdn/typeahead/{{from_lang}}/' + encodeURI(encodeURI(prefix.toLowerCase())) + '.txt', {
-                    success: function (data) {
-                        strs = data;
-                        var m = matches(q);
-                        cb_async(m);
-                        loaded_prefix = prefix;
-                    }
-                });
+            if (loaded_prefix !== prefix) {
+                merged_and_sorted = [];
+                loaded_prefix = undefined;
+                loaded_langs = {};
+                load_lang(from_lang, prefix, q, cb_async);
+                load_lang(to_lang, prefix, q, cb_async);
             }
 
-            cb(matches(q));
+            cb(match(q));
         };
     };
 
@@ -47,14 +68,15 @@ $(function() {
             minLength: 3
         },
         {
-            source: substringMatcher([]),
+            source: substringMatcher(),
             limit: 5,
             display: function (x) {
                 return x[0];
             },
             templates: {
                 suggestion: function (x) {
-                      return '<div>' + x[0] + ' &mdash; de</div>';
+                      return '<div class="suggestion clearfix"><span class="word">' + x[0]
+                                + '</span><span class="language text-muted">' + x[2] + '</span></div>';
                 }
             }
         });
