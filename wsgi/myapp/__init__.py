a4f618d9703165e5ed2fbc8587be31a5726c895b
Karl Bartel <karl42@gmail.com>
2016-01-20 21:10:21 +0100

    add summary page

--- wsgi/myapp/__init__.py
+++ wsgi/myapp/__init__.py
@@ -99,5 +99,28 @@ def show_user_agents():
     return base.render_template('admin/user_agents.html', rows=rows)
 
 
+@app.route('/admin/summary')
+def show_summary():
+    langs = base.db_query("logging", """
+        SELECT lang1, lang2, count(*) AS count
+        FROM search_log
+        WHERE user_agent NOT LIKE '%bot%'
+        GROUP BY 1, 2 ORDER BY 3 DESC
+        LIMIT 10
+    """, path='')
+    recent_searches = base.db_query("logging", """
+        SELECT DISTINCT lang1, lang2, query, results1 + results2 AS results
+        FROM search_log
+        WHERE user_agent NOT LIKE '%bot%'
+        ORDER BY ts DESC
+        LIMIT 50
+    """, path='')
+    return base.render_template('admin/summary.html',
+        langs=langs,
+        recent_hits=[x for x in recent_searches if x.results > 0],
+        recent_misses=[x for x in recent_searches if x.results == 0],
+    )
+
+
 if __name__ == "__main__":
     app.run()
