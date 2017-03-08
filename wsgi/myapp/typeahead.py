43b80f4eeb360aafa30f802d6ad9e9b8acace10a
Karl Bartel <karl42@gmail.com>
2017-03-08 18:57:58 +0100

    add suggestions to opensearch

--- wsgi/myapp/typeahead.py
+++ wsgi/myapp/typeahead.py
@@ -23,3 +23,26 @@ def typeahead(from_lang, to_lang, query):
         attach_dbs=dict(other=to_lang + '-' + from_lang)
     )
     return jsonify(rows)
+
+
+@app.route('/opensearch/typeahead/<from_lang>-<to_lang>/<path:query>')
+def typeahead_opensearch(from_lang, to_lang, query):
+    if len(query) < 3:
+        return jsonify([])
+    rows = db_query(from_lang + '-' + to_lang, """
+        SELECT written_rep, replace(trans_list, ' | ', ', ') AS trans_list, lang, max_score, rel_importance
+        FROM (
+            SELECT written_rep, trans_list, max_score, rel_importance, :from_lang AS lang
+            FROM simple_translation
+            WHERE written_rep LIKE :query || '%'
+            UNION ALL
+            SELECT written_rep, trans_list, max_score, rel_importance, :to_lang AS lang
+            FROM other.simple_translation
+            WHERE written_rep LIKE :query || '%'
+        )
+        ORDER BY max_score * rel_importance DESC
+        """,
+                    dict(query=query, from_lang=from_lang, to_lang=to_lang),
+                    attach_dbs=dict(other=to_lang + '-' + from_lang)
+                    )
+    return jsonify([query, [r[0] for r in rows], [r[1] for r in rows]])
