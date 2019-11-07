71cd2acf694f34e68af49f8ad84897fedd9be0ec
Karl Bartel <karl42@gmail.com>
2019-11-07 21:32:06 +0100

    Use git to determine asset revision
    This is necessary after the switch from hg to git.

--- wsgi/myapp/__init__.py
+++ wsgi/myapp/__init__.py
@@ -16,7 +16,7 @@ import myapp.base as base
 import myapp.typeahead
 
 ADMINS = ['karl42@gmail.com']
-ASSET_REVISION = check_output(['hg', 'id', '-i'], cwd=base.APP_ROOT)
+ASSET_REVISION = check_output('git describe --abbrev=12 --always --dirty=+'.split(' '), cwd=base.APP_ROOT)
 
 # simple cache busting for static files, since Flask-Assets is only usable for js, css, etc.
 @app.url_defaults
