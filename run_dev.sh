68708b84c8341d4e4f2e6dc0c233da8ee665d9e0
Karl Bartel <karl42@gmail.com>
2019-11-10 12:58:34 +0100

    Use ICU included in sqlite, but spellfix1 as extension

--- run_dev.sh
+++ run_dev.sh
@@ -2,6 +2,7 @@
 if [ ! -d venv ]; then
 	python3 -m venv venv
 	CFLAGS='-DSQLITE_ENABLE_ICU' CPPFLAGS=`pkg-config --cflags icu-uc icu-uc icu-i18n` LDFLAGS=`pkg-config --libs icu-uc icu-uc icu-i18n` venv/bin/pip install git+git://github.com/karlb/pysqlite3
+    python3 -c "`wget -qO- https://bitbucket.org/!api/2.0/snippets/karlb/j6gxM/38eff66f830e1c96b99e2ebb1312cae13c772c34/files/make_sqlite_ext.py`" wsgi/lib spellfix
 fi
 
 . venv/bin/activate
