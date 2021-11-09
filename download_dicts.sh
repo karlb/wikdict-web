6ee6a18bfa0bc4f2db4f42bd5d32ff23038ad769
Sebastian Pipping <sebastian@pipping.org>
2021-11-09 17:53:58 +0100

    download_dicts.sh: Adjust to current file system layout at download.wikdict.com

--- download_dicts.sh
+++ download_dicts.sh
@@ -1,2 +1,55 @@
-wget http://download.wikdict.com/dictionaries/sqlite/1/all.tar.gz -O data/all.tar.gz
-mkdir -p data/dict && tar xvfz data/all.tar.gz --directory data/dict && rm data/all.tar.gz
+#! /usr/bin/env bash
+# Copyright (c) 2021 Sebastian Pipping <sebastian@pipping.org>
+# Licensed under the MIT license, see file COPYING for the full text
+
+set -x
+set -e
+
+languages=(
+    bg
+    de
+    el
+    en
+    es
+    fi
+    fr
+    id
+    it
+    ja
+    la
+    lt
+    mg
+    nl
+    no
+    pl
+    pt
+    ru
+    sv
+    tr
+)
+
+language_pairs=()
+for language_from in "${languages[@]}"; do
+    for language_to in "${languages[@]}"; do
+        if [[ "${language_from}" = "${language_to}" ]]; then
+            continue
+        fi
+
+        language_pairs+=( "${language_from}-${language_to}" )
+    done
+done
+
+
+mkdir -p data/dict
+
+
+for name in wikdict "${languages[@]}" "${language_pairs[@]}"; do
+    basename="${name}.sqlite3"
+    filename="data/dict/${basename}"
+
+    if [[ -f "${filename}" ]]; then
+        continue
+    fi
+
+    wget -O "${filename}" "https://download.wikdict.com/dictionaries/wdweb/${basename}"
+done
