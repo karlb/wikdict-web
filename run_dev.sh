2cc1f93ed48ace832d8fb068571856b26d3c2f50
Karl Bartel <karl@karl.berlin>
2025-01-25 13:08:13 +0100

    Use uv to manage python project

--- run_dev.sh
+++ run_dev.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
-if [ ! -d venv ]; then
-	python3 -m venv venv
-fi
-
-. venv/bin/activate
 
 set -o allexport
 source development_env.sh
 set +o allexport
 
-pip install --quiet -r requirements.txt
 if [[ $1 == test ]]; then
-    python3 -m unittest tests/web_tests.py
+    uv run python -m unittest tests/web_tests.py
 else
-    python3 -m wikdict_web
+    uv run python -m wikdict_web
 fi
